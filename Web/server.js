9d2df60bb7f9f2b76ae9ab0d79b6e8905af3b87b
pabgre <pablosan@tutanota.com>
2020-11-09 18:14:19 +0100

    Basic structure

--- Web/server.js
+++ Web/server.js
@@ -9,13 +9,21 @@ app.set('view engine', 'ejs');
 app.use(express.static("public"));
 
 app.get("/", function(req, res){
-  res.render("index", {PageTitle: "Wellcome"});
+  //res.render("index", {PageTitle: "Wellcome"});
+  res.redirect("/machines-turnabout");
 })
 
 app.get("/machines-turnabout", function(req, res){
-  res.render("pages/machines", {PageTitle: "The Machine Turnabout"});
+  const msgs = require("./language/machines/en.json");
+  res.render("pages/machines", {msg:msgs});
 })
 
-app.listen(3000, function(){
+app.get("/juicio-de-las-maquinas", function(req, res){
+  const msgs = require("./language/machines/es.json");
+  res.render("pages/machines", {msg:msgs});
+})
+
+
+app.listen(process.env.PORT || 3000, function(){
   console.log("Server started on port 3000");
 });
